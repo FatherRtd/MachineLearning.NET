b35f642e3b9ec4e69850508f0f9ea672bccdbf68
Евгений Евгений <berezin.evgenij.01@mail.ru>
2024-09-26 23:35:43 +0500

    lab 1

---
+++ Lab1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Lab1.data;
+using Plotly.NET.CSharp;
+using GenericChart = Plotly.NET.GenericChart;
+
+Func<float, float> function = (x) => 2 * x + 5;
+
+var dataGenerator = new DataGenerator(123123, function);
+var data = dataGenerator.CreateIncreasingSequence(20, 0, 10).ToArray();
+var toPredict = data.Skip(10).Take(10).ToArray();
+
+for (int i = 0; i < toPredict.Count(); i++)
+{
+	var sampleData = new Lab1.Lab1.ModelInput
+	{
+		X = toPredict[i].Item1
+	};
+
+	var result = Lab1.Lab1.Predict(sampleData);
+
+	toPredict[i].Item2 = result.Score;
+}
+
+
+
+Chart.Combine(new List<GenericChart>()
+	{
+		Chart.Line<float, float, string>(x: data.Select(x => x.Item1), y: data.Select(x => x.Item2),
+			Name: "Real"),
+		Chart.Line<float, float, string>(x: toPredict.Select(x => x.Item1), y: toPredict.Select(x => x.Item2),
+			Name: "Predicted")
+	})
+	.WithXAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("xAxis"))
+	.WithYAxisStyle<double, double, string>(Title: Plotly.NET.Title.init("yAxis"))
+	.Show();
---
+++ Lab1/data/DataGenerator.cs
@@ -0,0 +1,31 @@
+﻿namespace Lab1.data
+{
+    public class DataGenerator
+    {
+        private readonly Random _random;
+        private readonly Func<float, float> _dataFunction;
+
+        public DataGenerator(int seed, Func<float, float> dataFunction)
+        {
+            _dataFunction = dataFunction;
+            _random = new Random(seed);
+        }
+
+        private float GetRandom(double min, double max)
+        {
+            return (float)(_random.NextDouble() * (max - min) + min);
+        }
+
+        public IEnumerable<(float, float)> CreateIncreasingSequence(int count, float minValue, float maxValue)
+        {
+            float intervalDelta = (maxValue - minValue) / count;
+            float min = minValue;
+            for (int i = 0; i < count; i++, min += intervalDelta)
+            {
+                var x = GetRandom(min, min + intervalDelta);
+                var y = _dataFunction(x);
+                yield return (x, y);
+            }
+        }
+    }
+}
