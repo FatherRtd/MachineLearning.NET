9065d42383ee63cdcbbfed95d4e88a3dfddcd3ed
FatherRtd <79792471+FatherRtd@users.noreply.github.com>
2024-09-27 20:25:03 +0500

    lab2

---
+++ Lab2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using Lab2;
+
+var path = @"C:\source\MLTest\Lab2\data\kot-eti-udivitelnye-kotiki.jpg";
+
+var src = File.ReadAllBytes(path);
+var modelInput = new MLModel.ModelInput
+{
+    ImageSource = src
+};
+
+var result = Lab2.MLModel.Predict(modelInput);
+
+var scores = Lab2.MLModel.GetSortedScoresWithLabels(result);
+
+foreach (var (key, value) in scores)
+{
+    Console.WriteLine($"{key}: {value:F}");
+}
